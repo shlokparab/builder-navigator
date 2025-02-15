f6b16f95a28a56c4532f3677310e75e3cd0a07d6
akshaykripalani <164727097+akshaykripalani@users.noreply.github.com>
2025-02-16 02:16:09 +0530

    fix minor

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ load_dotenv()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],  # Frontend URL
+    allow_origins=["*"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
